e363d3592fa6228a96634b1e34a5fb7d96ab8ecf
fernandorohr <fernando.felipe.flesch@gmail.com>
2020-11-03 11:47:37 -0300

    Strating implementation of board and piece

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,14 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Position pos = new Position(5, 3);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
+
+        System.out.println("Work in Progress!");
     }
 }
