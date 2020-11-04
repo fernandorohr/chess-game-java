db10bf3fc6bce15b99fbb28f08f24bbecf905e88
fernandorohr <fernando.felipe.flesch@gmail.com>
2020-11-04 09:42:06 -0300

    Creating chess exceptions e converting chess position

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -26,8 +25,13 @@ public class ChessMatch {
         return mat;
     }
 
+    private void placeNewPiece (char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column,row).toPosition());
+    }
+
     public void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(7, 0));
-        board.placePiece(new King(board, Color.WHITE), new Position(2, 2));
+        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 }
